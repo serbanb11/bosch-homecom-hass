6d7749a0f9f32f4568ce67f9eafbdbd5f9ed3753
serbanb11 <serban_b@ymail.com>
2025-09-14 11:43:29 +0300

    update water heater

--- custom_components/bosch_homecom/water_heater.py
+++ custom_components/bosch_homecom/water_heater.py
@@ -25,9 +25,13 @@ async def async_setup_entry(
 
     for coordinator in coordinators:
         if coordinator.data.device["deviceType"] == "wddw2":
-            entities.append(
-                BoschComWddw2WaterHeater(coordinator=coordinator, field="waterheater")
-            )
+            for ref in coordinator.data.dhw_circuits:
+                dhw_id = ref["id"].split("/")[-1]
+                if re.fullmatch(r"dhw\d", dhw_id):
+                    entities.append(
+                        BoschComWddw2WaterHeater(
+                            coordinator=coordinator, field=dhw_id
+                        )
         elif (
             coordinator.data.device["deviceType"] == "k40"
             or coordinator.data.device["deviceType"] == "k30"
@@ -114,9 +118,11 @@ class BoschComWddw2WaterHeater(CoordinatorEntity, WaterHeaterEntity):
     """Representation of a BoschComWddw2 water heater entity."""
 
     _attr_has_entity_name = True
-    _attr_name = None
     _attr_temperature_unit = UnitOfTemperature.CELSIUS
-    _attr_supported_features = WaterHeaterEntityFeature.OPERATION_MODE
+    _attr_supported_features = (
+        WaterHeaterEntityFeature.OPERATION_MODE
+        | WaterHeaterEntityFeature.TARGET_TEMPERATURE
+    )
 
     def __init__(
         self,
@@ -125,12 +131,13 @@ class BoschComWddw2WaterHeater(CoordinatorEntity, WaterHeaterEntity):
     ) -> None:
         """Initialize water heater entity."""
         super().__init__(coordinator)
-        # self._attr_translation_key = "ac"
+        self._attr_translation_key = "dhw"
         self._attr_device_info = coordinator.device_info
-        self._attr_unique_id = f"{coordinator.unique_id}"
-        self._attr_name = field
+        self._attr_unique_id = f"{coordinator.unique_id}-{field}-waterheater"
+        self._attr_name = field + "_waterheater"
         self._coordinator = coordinator
         self._attr_should_poll = False
+        self.field = field
 
         # Call this in __init__ so data is populated right away, since it's
         # already available in the coordinator data.
@@ -146,10 +153,23 @@ class BoschComWddw2WaterHeater(CoordinatorEntity, WaterHeaterEntity):
         """Set new target operation mode."""
         for ref in self.coordinator.data.dhw_circuits:
             dhw_id = ref["id"].split("/")[-1]
-            if re.fullmatch(r"dhw\d", dhw_id):
+            if dhw_id == self.field:
                 await self.coordinator.bhc.async_put_dhw_operation_mode(
                     self._attr_unique_id, dhw_id, operation_mode
                 )
+        await self.coordinator.async_request_refresh()
+
+    async def async_set_temperature(self, **kwargs: Any) -> None:
+        """Set new target temperature."""
+        if (temperature := kwargs.get(ATTR_TEMPERATURE)) is None:
+            return
+        for ref in self.coordinator.data.dhw_circuits:
+            dhw_id = ref["id"].split("/")[-1]
+            if dhw_id == self.field:
+                await self.coordinator.bhc.async_set_dhw_temp_level(
+                    self._attr_unique_id, dhw_id, "manual", temperature
+                )
+
         await self.coordinator.async_request_refresh()
 
     def _set_domestic_hot_water_circuits(
@@ -159,7 +179,7 @@ class BoschComWddw2WaterHeater(CoordinatorEntity, WaterHeaterEntity):
 
         for ref in domestic_hot_water_circuits:
             dhw_id = ref["id"].split("/")[-1]
-            if re.fullmatch(r"dhw\d", dhw_id):
+            if dhw_id == self.field:
                 for key in ref:
                     match key:
                         case "operationMode":
@@ -170,6 +190,16 @@ class BoschComWddw2WaterHeater(CoordinatorEntity, WaterHeaterEntity):
                                 .get("value", "unknown")
                             )
                             self._attr_operation_list = ref[key]["allowedValues"]
+                            self._attr_min_temp = (
+                                (ref.get("tempLevel") or {})
+                                .get("manual", {})
+                                .get("minValue", "36")
+                            )
+                            self._attr_max_temp = (
+                                (ref.get("tempLevel") or {})
+                                .get("manual", {})
+                                .get("maxValue", "60")
+                            )
         self._attr_current_operation = operationMode_value
         self._attr_current_temperature = actualTemp_value
 
