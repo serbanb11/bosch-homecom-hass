5c2d10fc1b761c91102b8717bb66eb629c7b9746
serbanb11 <serban_b@ymail.com>
2026-01-19 00:38:49 +0200

    fix k40 climate entity

--- custom_components/bosch_homecom/climate.py
+++ custom_components/bosch_homecom/climate.py
@@ -101,11 +101,11 @@ class BoschComRacClimate(CoordinatorEntity, ClimateEntity):
         self._attr_device_info = coordinator.device_info
         self._attr_unique_id = f"{coordinator.unique_id}"
         self._attr_name = field
-        self._coordinator = coordinator
         self._attr_should_poll = False
 
         # Call this in __init__ so data is populated right away, since it's
         # already available in the coordinator data.
+        if not coordinator.data: return
         self.set_attr()
 
     @callback
@@ -273,18 +273,18 @@ class BoschComRacClimate(CoordinatorEntity, ClimateEntity):
                 case _:
                     pass
 
-            if not hasattr(self, "_attr_target_temperature_high"):
-                self._attr_target_temperature = 22
-                self._attr_target_temperature_high = 30
-                self._attr_target_temperature_low = 16
+        if not hasattr(self, "_attr_target_temperature_high"):
+            self._attr_target_temperature = 22
+            self._attr_target_temperature_high = 30
+            self._attr_target_temperature_low = 16
 
     def set_attr(self) -> None:
         """Populate attributes with data from the coordinator."""
         self._set_standard_functions(self.coordinator.data.stardard_functions)
 
+        self._attr_preset_mode = PRESET_NONE
         for ref in self.coordinator.data.advanced_functions:
             normalized_id = ref["id"].split("/", 2)[-1]
-            self._attr_preset_mode = PRESET_NONE
 
             match normalized_id:
                 case "fullPowerMode":
@@ -319,11 +319,10 @@ class BoschComK40Climate(CoordinatorEntity, ClimateEntity):
     ) -> None:
         """Initialize climate entity."""
         super().__init__(coordinator)
-        self._attr_translation_key = "ac"
+        self._attr_translation_key = "hc"
         self._attr_device_info = coordinator.device_info
-        self._attr_unique_id = f"{coordinator.unique_id}"
+        self._attr_unique_id = f"{coordinator.unique_id}-{field}"
         self._attr_name = field
-        self._coordinator = coordinator
         self._attr_should_poll = False
 
         # Call this in __init__ so data is populated right away, since it's
@@ -341,16 +340,9 @@ class BoschComK40Climate(CoordinatorEntity, ClimateEntity):
         if (temperature := kwargs.get(ATTR_TEMPERATURE)) is None:
             return
 
-        heating_circuits = await self.coordinator.bhc.async_get_hc(self._attr_unique_id)
-        references = heating_circuits.get("references", [])
-        if not references:
-            return
-
-        for ref in references:
-            hc_id = ref["id"].split("/")[-1]
-            await self.coordinator.bhc.async_set_hc_manual_room_setpoint(
-                self._attr_unique_id, hc_id, temperature
-            )
+        await self.coordinator.bhc.async_set_hc_manual_room_setpoint(
+            self._attr_unique_id, self._attr_name, temperature
+        )
 
         await self.coordinator.async_request_refresh()
 
@@ -363,50 +355,50 @@ class BoschComK40Climate(CoordinatorEntity, ClimateEntity):
 
         await self.coordinator.async_request_refresh()
 
-    def _set_heating_circuits(self, heating_circuits: list[dict]) -> None:
+    def _set_heating_circuits(self, heating_circuits: dict) -> None:
         """Populate heating circuits."""
 
-        for ref in heating_circuits:
-            for key in ref:
-                val = ref[key]
-                if not isinstance(val, dict):
-                    continue
-                if val.get("value") is None:
-                    continue
-                match key:
-                    case "operationMode":
-                        match ref[key]["value"]:
-                            case "auto":
-                                self._attr_hvac_mode = HVACMode.AUTO
-                            case "off":
-                                self._attr_hvac_mode = HVACMode.OFF
-                            case "manual":
-                                # Not sure what to do here
-                                self._attr_hvac_mode = HVACMode.AUTO
-                    case "currentSuWiMode":
-                        match ref[key]["value"]:
-                            case "off":
-                                self._attr_hvac_action = HVACAction.IDLE
-                            case "forced":
-                                self._attr_hvac_action = HVACAction.HEATING
-                            case "cooling":
-                                self._attr_hvac_action = HVACAction.COOLING
-                    case "currentRoomSetpoint":
-                        self._attr_target_temperature = ref[key]["value"]
-                    case "roomTemp":
-                        self._attr_current_temperature = ref[key]["value"]
-                    case "actualHumidity":
-                        self._attr_current_humidity = ref[key]["value"]
+        for key in heating_circuits:
+            val = heating_circuits[key]
+            if not isinstance(val, dict):
+                continue
+            if val.get("value") is None:
+                continue
+            match key:
+                case "operationMode":
+                    match heating_circuits[key]["value"]:
+                        case "auto":
+                            self._attr_hvac_mode = HVACMode.AUTO
+                        case "off":
+                            self._attr_hvac_mode = HVACMode.OFF
+                        case "manual":
+                            # Not sure what to do here
+                            self._attr_hvac_mode = HVACMode.AUTO
+                case "currentSuWiMode":
+                    match heating_circuits[key]["value"]:
+                        case "off":
+                            self._attr_hvac_action = HVACAction.IDLE
+                        case "forced":
+                            self._attr_hvac_action = HVACAction.HEATING
+                        case "cooling":
+                            self._attr_hvac_action = HVACAction.COOLING
+                case "currentRoomSetpoint":
+                    self._attr_target_temperature = heating_circuits[key]["value"]
+                case "roomTemp":
+                    self._attr_current_temperature = heating_circuits[key]["value"]
+                case "actualHumidity":
+                    self._attr_current_humidity = heating_circuits[key]["value"]
 
     def set_attr(self) -> None:
         """Populate attributes with data from the coordinator."""
-        # self._set_standard_functions(self.coordinator.data.stardard_functions)
-        self._set_heating_circuits(self.coordinator.data.heating_circuits)
+        data = self.coordinator.data
+        if not data:
+            return
 
-        match self.coordinator.data.away_mode.get("value"):
-            case "off":
-                self._attr_preset_mode = PRESET_NONE
-            case "on":
-                self._attr_preset_mode = PRESET_AWAY
-            case _:
-                self._attr_preset_mode = PRESET_NONE
+        for entry in (data.heating_circuits or []):
+            if entry.get("id") == f"/heatingCircuits/{self._attr_name}":
+                self._set_heating_circuits(entry)
+                break
+
+        away = (data.away_mode or {}).get("value")
+        self._attr_preset_mode = PRESET_AWAY if away == "on" else PRESET_NONE
